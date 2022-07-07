00f71f874c8178b27c6ed344f2ec36786280ab06
Udhayarajan J <udhayarajan.j@shauryatechnosoft.com>
2022-07-07 11:04:50 +0530

    Add project files.

---
+++ AppCode/AppSecurityUtility.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AppsecurityWithSaltAndHash.AppCode
+{
+    public class AppSecurityUtility
+    {
+        private static AppSecurityUtility? _instance;
+        public static AppSecurityUtility Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new AppSecurityUtility();
+                return _instance;
+            }
+        }
+        public byte[] GenerateSalt(int userDefinedSize)
+        {
+            byte[] saltBytes = new byte[userDefinedSize];
+#pragma warning disable SYSLIB0023
+            using (var rSACryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                rSACryptoServiceProvider.GetBytes(saltBytes);
+            }
+#pragma warning restore SYSLIB0023
+            return saltBytes;
+        }
+        public string HashAlgorithm(string password, int lengthOfSaltSize, HashAlgorithm hashAlgorithm)
+        {
+            byte[] RandomSaltByte = GenerateSalt(lengthOfSaltSize);
+            byte[] saltBytes = Encoding.UTF8.GetBytes(password);
+            List<byte> combileByte = new List<byte>();
+            combileByte.AddRange(RandomSaltByte);
+            combileByte.AddRange(saltBytes);
+            byte[] result = hashAlgorithm.ComputeHash(combileByte.ToArray());
+            return Convert.ToBase64String(result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppsecurityWithSaltAndHash.AppCode;
+using System.Security.Cryptography;
+
+string inputPassword = string.Empty;
+AppSecurityUtility appSecurityUtility = AppSecurityUtility.Instance;
+Console.Write("\n Enter the Input Password : ");
+inputPassword=Console.ReadLine();
+string Encryption = appSecurityUtility.HashAlgorithm(inputPassword, 64, SHA256.Create());
+Console.WriteLine(Encryption);
